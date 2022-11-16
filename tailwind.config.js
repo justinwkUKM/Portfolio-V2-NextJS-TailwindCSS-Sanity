fd5f63c1ffe7f810ee4928ffc32a944f140ba511
TheLorry <thelorry@Admins-MacBook-Pro-3.local>
2022-11-16 16:58:08 +0800

    added custom wiggle animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'bounce-slow': 'bounce 3s linear infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+
+      }
+    },
   },
   plugins: [require('tailwind-scrollbar'),
 ],
