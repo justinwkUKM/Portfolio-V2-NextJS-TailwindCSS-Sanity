22011d9131e81102e30e97789e696fdb7ee4c1de
TheLorry <thelorry@Admins-MacBook-Pro-3.local>
2022-11-18 12:22:17 +0800

    added header hover color

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,18 +17,18 @@ function Header({ socials, pageInfo }: Props) {
 
         if (currentTheme === 'dark') {
             return (
-                <button onClick={() => setTheme('light')}>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:text-orange-300 hover:scale-110 animate-wiggle">
+                <button className='hover:scale-110' onClick={() => setTheme('light')}>
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:text-orange-300  animate-wiggle">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                     </svg>
                 </button>
             )
         } else {
             return (
-                <button onClick={() => setTheme('dark')}>
+                <button className='hover:scale-110'  onClick={() => setTheme('dark')}>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:text-yellow-600 hover:scale-110 animate-wiggle">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:text-yellow-600  animate-wiggle">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                     </svg>
                 </button>
@@ -53,13 +53,13 @@ function Header({ socials, pageInfo }: Props) {
                 }}
                 className='flex flex-row items-center '>
                 
-                <div className='md:inline-flex  hidden md:visible'>
+                <div className='md:inline-flex  hidden md:visible '>
                     <SocialIcon className='hover:animate-[spin_1s_ease-in-out] hover:bg-orange-400  dark:hover:bg-gray-200 rounded-full' url='#hero' network='vsco' label="Our portfolio" fgColor='gray' bgColor='transparent' />
                 </div>
                 <div className=''>
 
                     {socials.map((social, i) => (
-                        <SocialIcon className='hover:animate-[spin_1s_ease-in-out] hover:bg-orange-400  dark:hover:bg-gray-200 rounded-full' key={social._id} url={social.url} fgColor='gray' bgColor='transparent' />
+                        <SocialIcon className='hover:animate-[spin_1s_ease-in-out] hover:bg-orange-400  dark:hover:bg-gray-200  rounded-full' key={social._id} url={social.url} fgColor='gray' bgColor='transparent' />
                     ))}
                 </div>
 
@@ -83,8 +83,8 @@ function Header({ socials, pageInfo }: Props) {
                         duration: 1
                     }}
                     className='flex text-gray-500 cursor-pointer pr-2 space-x-2 '>
-                    <button className='uppercase md:inline-flex font-semibold hidden md:visible'>CV</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:animate-bounce ">
+                    <button className='uppercase md:inline-flex font-semibold hidden md:visible hover:scale-110'>CV</button>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:animate-bounce hover:text-orange-300 ">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                     </svg>
                 </motion.div>
@@ -110,7 +110,7 @@ function Header({ socials, pageInfo }: Props) {
                         }}
                         className='flex text-gray-500 cursor-pointer pr-2 space-x-2 hover:scale-110 '>
                         <button className='uppercase md:inline-flex font-semibold hidden md:visible'>Get in touch</button>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:animate-[spin_1s_ease-in-out] ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 hover:animate-[spin_1s_ease-in-out] hover:text-orange-300 ">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                         </svg>
                     </motion.div>
