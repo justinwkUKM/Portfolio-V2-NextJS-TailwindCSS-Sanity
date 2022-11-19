fe6f8017fc933a8422c05c47ed88590e08616f63
TheLorry <thelorry@Admins-MacBook-Pro-3.local>
2022-11-19 11:27:10 +0800

    added PWA functionality

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   reactStrictMode: true,
+// //   swcMinify: true,
+// // }
+
+// // module.exports = nextConfig
+
+// const withPWA = require('next-pwa')
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa:{
+//     dest:"public",
+//     register:true,
+//     skipWaiting:true,
+//     disable:process.env.NODE_ENV === 'development'
+//   }
+// });
