5bc0e22b9448b81e656e7cf9918297bc2bb0062a
TheLorry <thelorry@Admins-MacBook-Pro-3.local>
2022-12-06 21:03:56 +0800

    added skill title

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -4,42 +4,62 @@ import { Skill as SkillType } from '../typings';
 import { urlFor } from '../sanity';
 type Props = {
     directionLeft?: boolean,
-    skill:SkillType
+    skill: SkillType
 }
 
 export default function Skill({ directionLeft, skill }: Props) {
     return (
 
-        <div className='group relative flex cursor-pointer hover:animate-wiggle-3s'>
-            <motion.img
-                initial={{
-                    // x: directionLeft ? -200 : 200,
-                    opacity: 0
-                }}
-                whileInView={{
-                    opacity:1, x:0,
-                }}
-                viewport={{
-                    once: true,
-                }}
-                transition={{
-                    duration: 2
-                }}
-                className='rounded p-1 bg-gray-100 border-2 border-gray-300 dark:bg-gray-100  object-cover 
-                w-12 h-12 xl:w-24 xl:h-24 filter  
+        <div className='group flex flex-col justify-center items-center '>
+
+            <div className='group relative flex cursor-pointer hover:animate-wiggle-3s'>
+                <motion.img
+                    initial={{
+                        // x: directionLeft ? -200 : 200,
+                        opacity: 0
+                    }}
+                    whileInView={{
+                        opacity: 1, x: 0,
+                    }}
+                    viewport={{
+                        once: true,
+                    }}
+                    transition={{
+                        duration: 2
+                    }}
+                    className='rounded p-1 bg-gray-100 border-2 border-gray-300 dark:bg-gray-100  object-cover 
+                w-12 h-12 xl:w-24 xl:h-24 filter ease-in-out duration-300 
                 '
-                src={urlFor(skill.image).url()}>
+                    src={urlFor(skill.image).url()}>
 
-            </motion.img>
+                </motion.img>
 
-            <div className='absolute opacity-0 group-hover:opacity-70 transition 
-            ease-in-out group-hover:bg-gray-300 w-12 h-12 xl:w-24 xl:h-24 rounded'>
-                <div className='flex h-full items-center justify-center'>
-                    <p className='text-sm lg:text-2xl text-black font-bold opacity-100'>
-                        {skill.progress}%
-                    </p>
+                <div className='absolute opacity-0 group-hover:opacity-70 transition 
+            ease-in-out group-hover:bg-gray-300 w-12 h-12 xl:w-24 xl:h-24 rounded '>
+                    <div className='flex h-full items-center justify-center'>
+                        <p className='text-sm lg:text-2xl text-black font-bold opacity-100'>
+                            {skill.progress}%
+                        </p>
+                    </div>
                 </div>
+
             </div>
+            <motion.div
+                    initial={{
+                        y:-150,
+                        opacity: 0
+                    }}
+                    whileInView={{
+                        opacity: 1,
+                        y:0,
+
+                    }}
+                    
+                    transition={{
+                        duration: 0.5
+                    }} className='text-gray-500 hidden group-hover:inline-flex group-hover:visible group-hover:opacity-70 ease-in-out duration-300'>
+                <h1 className='text-xs'>{skill.title}</h1>
+            </motion.div>
         </div>
     )
 }
